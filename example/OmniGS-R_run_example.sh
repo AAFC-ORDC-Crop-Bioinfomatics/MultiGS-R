f60f4ff2ecc58f6336f1459a4db89ee8aaabda59
Chunfang Zheng <chunfang.zheng@agr.gc.ca>
2025-10-17 11:17:25 -0400

    new path in job file

--- example/OmniGS-R_run_example.sh
+++ example/OmniGS-R_run_example.sh
@@ -1,8 +1,4 @@
-#$ -cwd
-#$ -pe smp 7
+java -version
+R --version
 
-export R_LIBS="/home/AAFC-AAC/zhengc/R/x86_64-pc-linux-gnu-library/3.5:/opt/R/lib64/R/library"
-src_path=../src/OmniGS-R
-java_path=/isilon/ottawa-rdc/users/youf/backup_data/sharedTool/GS_new_pipelines/GS_java/java/bin
-
-$java_path/java -Xmx2000g -jar $src_path/OmniGS-R.jar  gs_parameters_example.config
+java -Xmx2000g -jar ../src/OmniGS-R/OmniGS-R.jar  gs_parameters_example.config
