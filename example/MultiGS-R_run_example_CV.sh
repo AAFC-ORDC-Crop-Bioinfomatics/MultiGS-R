d3a01e5cede313d5f2267202adf2184c29f00091
Chunfang Zheng <chunfang.zheng@agr.gc.ca>
2025-10-31 11:47:26 -0400

    multi traits

---
+++ example/MultiGS-R_run_example_CV.sh
@@ -0,0 +1,4 @@
+java -version
+R --version
+
+java -Xmx2000g -jar ../pipeline/MultiGS-R/OmniGS-R.jar  gs_parameters_example_CV.config
--- example/OmniGS-R_run_example_CV.sh
+++
@@ -1,4 +0,0 @@
-java -version
-R --version
-
-java -Xmx2000g -jar ../pipeline/OmniGS-R/OmniGS-R.jar  gs_parameters_example_CV.config
